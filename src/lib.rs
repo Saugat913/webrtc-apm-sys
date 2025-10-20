2fda9d07cbdd7ab5943b5cf566a54ebff1cc4d50
Saugat Kandel <64123282+Saugat913@users.noreply.github.com>
2025-10-20 06:01:48 +0545

    [tryfix] linkage error for dependent

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[link(name = "webrtc_audio_processing", kind = "static")]
+unsafe extern "C" {}
+
 // Include generated bindings
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
